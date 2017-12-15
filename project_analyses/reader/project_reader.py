78cc82e6a4926338fe2edbea1674f9e2651c6d35
Paulo Batista <pauloc@linuxmail.org>
2017-12-14 23:46:45 -0200

    java reader has been added

--- project_analyses/reader/project_reader.py
+++ project_analyses/reader/project_reader.py
@@ -5,37 +5,54 @@ import pandas as pd
 class ProjectReader:
 
     def __init__(self, root, language):
+       
         self.root = root
         self.language = language
 
 
     def getProjectStructure(self):
+        
         structureList = []
         for path, subdirs, files in os.walk(self.root):
             for fileName in files:
                 structure = os.path.join(path, fileName)
                 self.filterProjectLanguage(structure, structureList)
-                #self.filterPythonProject(os.path.join(path, name))
-                #structure.append(os.path.join(path, name))
+
         return structureList
 
 
     def filterProjectLanguage(self, structure, structureList=[]):
+        
         if "python" in self.language:
             self.filterPythonProject(structure, structureList)
+        elif "java" in self.language:
+            self.filterJavaProject(structure, structureList)
 
 
 
     def filterPythonProject(self, structure, structureList=[]):
+       
         pythonPattern = re.compile(".+\.py$")
-        pythonInitPattern = re.compile(".+__init__.py$")
-        pythonCachePattern = re.compile(".+__pycache__.*")
+        pythonInitPattern = re.compile(".*__init__.py$")
+        pythonCachePattern = re.compile(".*__pycache__.*")
 
         if pythonInitPattern.match(structure):
             return
+        
         elif pythonCachePattern.match(structure):
             return
+        
         elif pythonPattern.match(structure):
             structureList.append(structure)
 
 
+    def filterJavaProject(self, structure, structureList=[]):
+        
+        javaPattern = re.compile(".+\.java$")
+        javadocPattern = re.compile(".*package-info.java$")
+
+        if javadocPattern.match(structure):
+            return
+
+        elif javaPattern.match(structure):
+            structureList.append(structure)
