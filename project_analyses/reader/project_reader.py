e851bedbbb7648bf2134f5bc672ed97dbf567f7c
Paulo Batista <pauloc@linuxmail.org>
2017-12-14 22:30:32 -0200

    python project reader method has been developed

--- project_analyses/reader/project_reader.py
+++ project_analyses/reader/project_reader.py
@@ -4,16 +4,38 @@ import re
 import pandas as pd
 class ProjectReader:
 
-    def __init__(self, root):
+    def __init__(self, root, language):
         self.root = root
+        self.language = language
 
 
     def getProjectStructure(self):
-        structure = []
+        structureList = []
         for path, subdirs, files in os.walk(self.root):
-            for name in files:
-                structure.append(os.path.join(path, name))
-        return structure
+            for fileName in files:
+                structure = os.path.join(path, fileName)
+                self.filterProjectLanguage(structure, structureList)
+                #self.filterPythonProject(os.path.join(path, name))
+                #structure.append(os.path.join(path, name))
+        return structureList
 
 
-    
+    def filterProjectLanguage(self, structure, structureList=[]):
+        if "python" in self.language:
+            self.filterPythonProject(structure, structureList)
+
+
+
+    def filterPythonProject(self, structure, structureList=[]):
+        pythonPattern = re.compile(".+\.py$")
+        pythonInitPattern = re.compile(".+__init__.py$")
+        pythonCachePattern = re.compile(".+__pycache__.*")
+
+        if pythonInitPattern.match(structure):
+            return
+        elif pythonCachePattern.match(structure):
+            return
+        elif pythonPattern.match(structure):
+            structureList.append(structure)
+
+
