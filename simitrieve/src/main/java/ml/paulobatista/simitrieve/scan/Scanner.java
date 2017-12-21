82028601e8e6f03f91314bf94652421bc08abf34
Paulo Batista <pauloc@linuxmail.org>
2017-12-21 13:37:31 -0200

    method that scann all root project source folder has been implemented

--- simitrieve/src/main/java/ml/paulobatista/simitrieve/scan/Scanner.java
+++ simitrieve/src/main/java/ml/paulobatista/simitrieve/scan/Scanner.java
@@ -3,10 +3,44 @@
  */
 package ml.paulobatista.simitrieve.scan;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import ml.paulobatista.simitrieve.error.ErrorHandler;
+
 /**
  * @author paulo
  *
  */
 public class Scanner {
 
+	public List<File> getFiles(String root) {
+		File rootFile;
+		List<File> files = new ArrayList<File>();
+		try {
+			rootFile = new File(root);
+			if (rootFile.isDirectory()) {
+				File[] leavesFile = rootFile.listFiles();
+
+				for (File iteratorFile : leavesFile) {
+
+					if (iteratorFile.isDirectory()) {
+						root = iteratorFile.getAbsolutePath();
+						files.addAll(getFiles(root));
+					}
+
+					else {
+						files.add(iteratorFile);
+					}
+				}
+			}
+		} catch (NullPointerException ex) {
+
+			ErrorHandler.fileNotExist();
+
+		}
+
+		return files;
+	}
 }
