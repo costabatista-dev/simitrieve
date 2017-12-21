34320827a43ead4833f781365ca42dec4b356381
Paulo Batista <pauloc@linuxmail.org>
2017-12-21 16:45:15 -0200

    error handle added

--- simitrieve/src/main/java/ml/paulobatista/simitrieve/error/ErrorHandler.java
+++ simitrieve/src/main/java/ml/paulobatista/simitrieve/error/ErrorHandler.java
@@ -8,6 +8,22 @@ public class ErrorHandler {
 
 		System.out.println("Unexpected Error: Project file doesn't exist");
 
-		System.exit(1);
+		System.exit(Error.FILE_NOT_EXIST.getCode());
+	}
+	
+	public static void unexpectedProjectProgrammingLanguage() {
+		System.out.println("Unexpected Error: Project programming language haven't support");
+		System.exit(Error.PROJECT_PROGRAMMING_LANGUAGE_NOT_SUPPORTED.getCode());
+	}
+	
+	public static void patternIsNULL() {
+		System.out.println("Unexpected Error: Project pattern language file is NULL");
+		System.exit(Error.PROJECT_LANGUAGE_PATTERN_IS_NULL.getCode());
+	}
+	
+	public static void projectIsEmpty() {
+		System.out.println("Unexpected Error: For some reason project's empty, check source code root folder\n"
+				+ "It's possible that the programming language's wrong to this project");
+		System.exit(Error.PROJECT_IS_EMPTY.getCode());
 	}
 }
