23c818fb75d1baa4363918da5170e63afc5caba2
Paulo Batista <pauloc@linuxmail.org>
2017-12-21 19:06:15 -0200

    error when a file is readed werer added

--- simitrieve/src/main/java/ml/paulobatista/simitrieve/error/ErrorHandler.java
+++ simitrieve/src/main/java/ml/paulobatista/simitrieve/error/ErrorHandler.java
@@ -8,22 +8,33 @@ public class ErrorHandler {
 
 		System.out.println("Unexpected Error: Project file doesn't exist");
 
-		System.exit(Error.FILE_NOT_EXIST.getCode());
+		System.exit(Error.FILE_NOT_EXIST_ERROR.getCode());
 	}
 	
 	public static void unexpectedProjectProgrammingLanguage() {
 		System.out.println("Unexpected Error: Project programming language haven't support");
-		System.exit(Error.PROJECT_PROGRAMMING_LANGUAGE_NOT_SUPPORTED.getCode());
+		System.exit(Error.PROJECT_PROGRAMMING_LANGUAGE_NOT_SUPPORTED_ERROR.getCode());
 	}
 	
 	public static void patternIsNULL() {
 		System.out.println("Unexpected Error: Project pattern language file is NULL");
-		System.exit(Error.PROJECT_LANGUAGE_PATTERN_IS_NULL.getCode());
+		System.exit(Error.PROJECT_LANGUAGE_PATTERN_IS_NULL_ERROR.getCode());
 	}
 	
 	public static void projectIsEmpty() {
 		System.out.println("Unexpected Error: For some reason project's empty, check source code root folder\n"
 				+ "It's possible that the programming language's wrong to this project");
-		System.exit(Error.PROJECT_IS_EMPTY.getCode());
+		System.exit(Error.PROJECT_IS_EMPTY_ERROR.getCode());
+	}
+	
+	public static void errorInFileReader(String fileName, String line) {
+		System.out.println("Unexpected Error: Error while reading text from file - " + fileName + 
+				"in line " + line + " of it");
+		System.exit(Error.READING_FILE_ERROR.getCode());
+	}
+	
+	public static void UnexpectedFileExtension() {
+		System.out.println("Unexpected Error: A file with a unknown extension is inside the file list");
+		System.exit(Error.UNKNOWN_FILE_EXTENSION_ERROR.getCode());
 	}
 }
