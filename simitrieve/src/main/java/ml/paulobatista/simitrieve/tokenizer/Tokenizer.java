21b50759f7ccefa2c54971ce12adbb3bdbc70218
Paulo Batista <pauloc@linuxmail.org>
2018-01-04 12:02:15 -0200

    - cleaner has been added; - add dependency to pom.xml - add tokenizer methods;

--- simitrieve/src/main/java/ml/paulobatista/simitrieve/tokenizer/Tokenizer.java
+++ simitrieve/src/main/java/ml/paulobatista/simitrieve/tokenizer/Tokenizer.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import ml.paulobatista.simitrieve.filter.Cleaner;
+
 public class Tokenizer {
 	
 	
 	public List<String> tokenizeString(String text) {
-		StringTokenizer tokenizer = new StringTokenizer(text, "");
+		StringTokenizer tokenizer = new StringTokenizer(text, " ");
 		int size = tokenizer.countTokens();
 		
 		List<String> tokens = new ArrayList<String>();
@@ -16,8 +18,30 @@ public class Tokenizer {
 		for(int index = 0; index < size; index++ ) {
 			tokens.add(tokenizer.nextToken());
 		}
+		
+		Cleaner cleaner = new Cleaner();
+		
+		tokens = cleaner.removeNonWordCharacters(tokens);
+		
 		return tokens;
 	}
 	
+	
+	public List<String> tokenize(List<String> sourceCode) {
+		List<String> tokenized = new ArrayList<String>();
+		
+		for(String line : sourceCode) {
+			
+			tokenized.addAll(tokenizeString(line));
+		}
+		
+		Cleaner cleaner = new Cleaner();
+		
+		tokenized = cleaner.removeBlanckLine(tokenized);
+		tokenized = cleaner.removeDigits(tokenized);
+		return tokenized;
+	}
+	
+	//public List<String> tokenize(List<String> sourceCode, camelcase)
 	
 }
