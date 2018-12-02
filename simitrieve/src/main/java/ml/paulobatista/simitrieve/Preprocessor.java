fca06acbd4ecd5a5c07a88bd19c2322c0c1154fd
Paulo Batista <pauloc@linuxmail.org>
2018-12-02 16:49:27 -0200

    a preprocessor java class was added to the project.

---
+++ simitrieve/src/main/java/ml/paulobatista/simitrieve/Preprocessor.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ml.paulobatista.simitrieve;
+
+/**
+ * @author paulo
+ *
+ */
+public class Preprocessor {
+	//this class will implement the logic of preprocessing methods.
+	
+	public Project removeComments(Project project) {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+	
+	public Project removeStopwords(Project project) {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+	
+
+}
