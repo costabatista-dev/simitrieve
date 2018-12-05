108cf6ac51f7b58f46902874dc5ee1e70ef10a65
Paulo Batista <pauloc@linuxmail.org>
2018-12-05 09:37:29 -0200

    methods to handle with arguments passed by command line added.

--- simitrieve/src/main/java/ml/paulobatista/simitrieve/App.java
+++ simitrieve/src/main/java/ml/paulobatista/simitrieve/App.java
@@ -24,25 +24,6 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Project project = new Project("agera", new String[] { "java" });
-		CommentRemover cr = new CommentRemover();
-		cr.removeComments(project);
-		Preprocessor pcp = new Preprocessor();
-		pcp.removeSpecialCharacters(project);
-		pcp.removeStopwords(project);
-		pcp.removeNonConceptWords(project);
-		CamelCaseSplitter ccs = new CamelCaseSplitter();
-		ccs.splitCamelCase(project);
-		pcp.tokenize(project);
-		pcp.removeLessFrequencyTerms(project, Quartile.THIRD);
-
-		for (ProgrammingFile pf : project) {
-			System.out.println(" ------------------ ");
-			System.out.println(pf.getPath());
-			for (String s : pf.getQuantifiedTerms().keySet()) {
-				System.out.println(s + " = " + pf.getQuantifiedTerms().get(s));
-			}
-		}
-	}
-
+		
+	
 }
